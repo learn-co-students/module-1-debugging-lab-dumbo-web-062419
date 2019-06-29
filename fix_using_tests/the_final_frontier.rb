ce6e6963167c80a832166545d05f88ee0ff262d4
github username <github email address>
2019-06-29 08:41:13 -0400

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -11,8 +12,10 @@ def crew
   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
-def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+def greet_crew(crew)
+  crew.map do |crew_member| 
+    "Hello #{crew_member}."
+  end
 end
 
 def engage
@@ -20,3 +23,4 @@ def engage
   state_log(date)
   greet_crew(crew)
 end
+
