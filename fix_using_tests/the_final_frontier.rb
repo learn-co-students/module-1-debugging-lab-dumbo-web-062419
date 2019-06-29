6aa2d580a5b36b2c51e21d70f9d077b250e793d2
John Fajardo <fajardocj@gmail.com>
2019-06-29 13:54:38 -0400

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -11,8 +11,14 @@ def crew
   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
-def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+def greet_crew(team)
+  fullgreet = []
+
+  team.each do |member|
+    fullgreet << "Hello #{member}."
+  end
+  
+  fullgreet
 end
 
 def engage
